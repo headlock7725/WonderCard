2425fdad41a2f37eeaa45b3ef8a21cb4be66610d
Stanislav Yefimenko <github.sprung132@passmail.net>
2025-04-25 19:10:40 +0200

    changes name header to WonderCard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">WonderCard</a>
       </div>
       <div className="flex-none">
         <a className="btn btn-flex btn-primary">Start Playing</a>
