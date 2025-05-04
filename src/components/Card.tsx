8582a07776c3fa94461161876ce771d70a9876e9
Stanislav Yefimenko <github.sprung132@passmail.net>
2025-05-04 23:14:23 +0200

    adds basic layout with cards and inputs for the game screen

---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+
+
+function Card() {
+  return (
+    <div className="shadow-gray-500 shadow-md white aspect-[24/36] rounded-xl max-w-64">
+      <img src="img/playing-card.webp" alt="Playing Card" className="w-full h-full object-center" />
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,10 +1,26 @@
-
+import Card from "../components/Card";
 
 
 function Game() {
   return (
-    <div className="grid grid-cols-9 grid-rows-9 h-full bg-base-200">
-      {/* Add your game components here */}
+    <div className="grid grid-cols-3 grid-rows-3 h-full w-full bg-base-200">
+      <div className="topic col-start-2 justify-self-center self-center">
+        <h3 className="text-3xl leading-9 font-bold">
+          Thema: Wissenschaftler
+        </h3>
+      </div>
+
+      <div className="cardContainer col-span-3 row-start-2 justify-self-center flex max-w-4/5 h-full gap-x-14 justify-between">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      <div className="resultInputs flex flex-row gap-x-5 conte col-start-2 row-start-3 justify-self-center self-center">
+        <input type="text" placeholder="Was kann das sein?" className="input input-neutral h-12 w-100" />
+        <a className="btn btn-flex btn-primary h-12">Wonder!</a>
+      </div>
     </div>
   );
 }
