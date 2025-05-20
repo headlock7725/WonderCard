a4755979e22b4da043e1f93a01f4a32696c83a44
Stanislav Yefimenko <github.sprung132@passmail.net>
2025-05-20 22:40:04 +0200

    adds Card params that are passed by Game.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,27 +1,37 @@
 
-function ActiveCard() {
+function ActiveCard({ title, image }: { title: string; image: string }) {
   return (
     <div className="flex h-full w-full flex-col justify-center items-center gap-6">
-      <img src="img/sets/physicists/Einstein.png" className="aspect-square max-w-32" />
-      <p className="text-lg leading-7 font-semibold">Albert Einstein</p>
+      <img src={image} className="aspect-square max-w-32" alt={title} />
+      <p className="text-lg leading-7 font-semibold">{title}</p>
     </div>
   );
 }
 
-
 function InactiveCard() {
   return (
-    <img src="img/playing-card.webp" alt="Playing Card" className="w-full h-full object-center" />
+    <img
+      src="img/playing-card.webp"
+      alt="Playing Card"
+      className="w-full h-full object-center"
+    />
   );
 }
 
-function Card({ isActive = false }) {
-  let content = null;
-  if (isActive) {
-    content = <ActiveCard />
-  } else {
-    content = <InactiveCard />
-  }
+function Card({
+  isActive = false,
+  title = "",
+  image = "",
+}: {
+  isActive?: boolean;
+  title?: string;
+  image?: string;
+}) {
+  const content = isActive ? (
+    <ActiveCard title={title} image={image} />
+  ) : (
+    <InactiveCard />
+  );
 
   return (
     <div className="shadow-gray-500 shadow-md white aspect-[24/36] rounded-xl max-w-64">
@@ -31,3 +41,4 @@ function Card({ isActive = false }) {
 }
 
 export default Card;
+
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,30 +1,38 @@
+import React from "react";
 import Card from "../components/Card";
-
+import content from "../config/content.json";
 
 function Game() {
+  const cards = content.topics[0].cards;
+
   return (
-    //TODO: add a countdown
-    < div className="grid grid-cols-3 grid-rows-3 h-full w-full bg-base-200" >
+    <div className="grid grid-cols-3 grid-rows-3 h-full w-full bg-base-200">
       <div className="topic col-start-2 justify-self-center self-center">
-        <h3 className="text-3xl leading-9 font-bold">
-          Topic: Physicists
-        </h3>
+        <h3 className="text-3xl leading-9 font-bold">Topic: Physicists</h3>
       </div>
 
       <div className="cardContainer col-span-3 row-start-2 justify-self-center flex max-w-4/5 h-full gap-x-14 justify-between">
-        <Card />
-        <Card />
-        <Card isActive={true} />
-        <Card />
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            isActive={card.shown}
+            title={card.title}
+            image={card.image}
+          />
+        ))}
       </div>
 
       <div className="resultInputs flex flex-row gap-x-5 conte col-start-2 row-start-3 justify-self-center self-center">
-        <input type="text" placeholder="What could that be?" className="input input-neutral h-12 w-100" />
+        <input
+          type="text"
+          placeholder="What could that be?"
+          className="input input-neutral h-12 w-100"
+        />
         <a className="btn btn-flex btn-primary h-12">Wonder!</a>
       </div>
-    </div >
+    </div>
   );
 }
 
-
 export default Game;
+
